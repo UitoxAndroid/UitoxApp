ce3693b21b93fc6221026ae75bb9ca14f17dfab4
babyandy <babyandy0111@gmail.com>
2015-08-23 01:01:53 +0800

    tset

---
+++ AndroidWorkSpace/asapapp/src/main/java/com/uitox/asap/GetTopMenu.java
@@ -0,0 +1,27 @@
+package com.uitox.asap;
+
+import com.uitox.asapapp.TopMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by babyandy on 2015/8/15.
+ */
+public class GetTopMenu {
+    private static List<TopMenu> selectedTop = new ArrayList<TopMenu>();
+
+    static {
+        selectedTop.add(new TopMenu("", "輪播 Demo", 0, "com.uitox.asap.BannerFragmentView"));
+        selectedTop.add(new TopMenu("", "ListView + ScrollView Demo", 0, "com.uitox.asap.ListViewANDScrollViewFragmentView"));
+        selectedTop.add(new TopMenu("", "AutoComplete Demo", 0, "com.uitox.asap.AutoCompleteTextFragmentView"));
+        selectedTop.add(new TopMenu("", "ListView", 0, "com.uitox.asap.ListViewFragmentView"));
+        selectedTop.add(new TopMenu("", "Map Demo", 0, "com.uitox.asap.MapsActivity"));
+        selectedTop.add(new TopMenu("", "多選單 Demo", 0, "com.uitox.asap.MultipleLevelFragmentView"));
+
+    }
+
+    public static List<TopMenu> getSelectedTopMenu() {
+        return selectedTop;
+    }
+}
---
+++ AndroidWorkSpace/asapapp/src/main/java/com/uitox/asap/MapsActivity.java
@@ -0,0 +1,97 @@
+package com.uitox.asap;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.uitox.view.BaseFragmentView;
+
+public class MapsActivity extends BaseFragmentView {
+
+    MapView mMapView;
+    private GoogleMap googleMap; // Might be null if Google Play services APK is not available.
+    private View view;
+
+    @Override
+    protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.map_location_info, container, false);
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+        // latitude and longitude
+        double latitude = 17.385044;
+        double longitude = 78.486671;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Hello Maps").snippet("Google Maps v2!");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(17.385044, 78.486671)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        return view;
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initBundle(Bundle bundle) {
+
+    }
+
+    @Override
+    public void updateUI() {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+
+}
